8661fdcd0740ad52f12ffe46e7cbaa94adfdccde
CodeSeba <31450548+CodeSeba@users.noreply.github.com>
2017-09-04 18:40:12 -0300

    Agregado un arreglo para llegar al mismo resultado

--- Clase-20170904/python-Sep04-ejr03.py
+++ Clase-20170904/python-Sep04-ejr03.py
@@ -6,9 +6,22 @@ n1 = int( input("Ingresar nro 1\n") )
 n2 = int( input("Ingresar nro 2\n") )
 
 if n1 == n2 :
-	print( "ERROR" )
+	print( "ERROR, los numeros son iguales." )
 else :
 	if n1 > n2 :
-		print( n1,"es mayor que",n2)
+		print( "El mayor es",n1 )
+		print( "El menor es",n2 )
 	else :
-		print( n2,"es mayor que",n1)
+		print( "El mayor es",n2 )
+		print( "El mayor es",n1 )
+		
+# Usando un arreglo y .sort(reverse=True)
+print( "Usando un Arreglo." )
+
+if n1 == n2 :
+	print( "ERROR, los numeros son iguales." )
+else :
+	a = [n1,n2]
+	a.sort(reverse=True)
+	print( "El mayor es",a[0] )
+	print( "El menor es",a[1] )
