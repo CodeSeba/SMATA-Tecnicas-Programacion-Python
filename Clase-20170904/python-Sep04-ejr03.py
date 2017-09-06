b915689957367ba6d516b3033592f9d486889772
CodeSeba <seba84x@gmail.com>
2017-09-06 10:30:28 -0300

    Ejercicio 03 hecho por la profe

--- Clase-20170904/python-Sep04-ejr03.py
+++ Clase-20170904/python-Sep04-ejr03.py
@@ -1,20 +1,21 @@
 # Ingresar 2 numeros por teclado
 # Indicar cual es el mayor y cual es el menor
 
+print( "Calculo del valos mayor entre 2 numeros" )
+
 n1 = int( input("Ingresar nro 1\n") )
 
 n2 = int( input("Ingresar nro 2\n") )
 
-if n1 == n2 :
-	print( "ERROR, los numeros son iguales." )
+if n1 > n2 :
+	print( "El menor es",n1 )
+	print( "El mayor es",n2 )
+elif n1 < n2 :
+	print( "El menor es",n2 )
+	print( "El mayor es",n1 )
 else :
-	if n1 < n2 :
-		print( "El menor es",n1 )
-		print( "El mayor es",n2 )
-	else :
-		print( "El menor es",n2 )
-		print( "El mayor es",n1 )
-		
+	print( "ERROR, los numeros son iguales." )
+
 # Usando un arreglo y .sort() que ordena de forma ascendente.
 print( "\nUsando un Arreglo.\n" )
 
