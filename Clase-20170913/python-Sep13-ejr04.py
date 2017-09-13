f45a684c50e3ffe159f0927e869aa67f215b9387
CodeSeba <seba84x@gmail.com>
2017-09-13 11:40:56 -0300

    Ejercicio 04

---
+++ Clase-20170913/python-Sep13-ejr04.py
@@ -0,0 +1,14 @@
+# Imprimir lo siguiente
+#	Comienzo
+#	0 x 0 = 0
+#	1 x 1 = 1
+#	2 x 2 = 4
+#	3 x 3 = 6
+#	4 x 4 = 16
+
+print("Comienzo")
+
+for i in range(5) :
+	print(i,"x",i,"=",i * i)
+
+print("Fin")
