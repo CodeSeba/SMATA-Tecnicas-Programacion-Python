c152af29801926de2d4e8c0be8b4eea2c8ada9c9
CodeSeba <seba84x@gmail.com>
2017-09-25 17:52:30 -0300

    Ejercicio 01

--- Clase-20170925/python-Sep25-ejr01.py
+++ Clase-20170925/python-Sep25-ejr01.py
@@ -2,64 +2,154 @@
 # Nombre
 # Sexo
 # Cant. de horas trabajadas
-# A. Se solicita imprimir los datos de cada empleado
+# A.
+# Se solicita imprimir los datos de cada empleado
 # Sabemos que la cant. de horas a trabajar es 160
 # y el valor de la hora es de 800
 # Se desea saber del empleado:
-# B. El sueldo bruto (160 x 800)
-# C. La cant. de horas extras.
+# B.
+# El sueldo basico (160 x 800)
+# C.
+# La cant. de horas extras.
 # La hora extra es un 50% mas que la hora comun.
-# D. Calcular el sueldo bruto incluyendo las horas extras.
-# E. Se debe abonar salario familiar, para eso debemos
+# D.
+# Calcular el sueldo basico mas las horas extras.
+# Se debe abonar salario familiar, para eso debemos
 # cargar:
 # Estado Civil
 # Cantidad de hijos
-# F. Si estado civil igual casado se paga por conyuge 200
-# G. Por hijo se paga 1000 y por cada hijo a partir del 3ro
-# se paga un 40% mas (de los 1000) por cada hijo.
+# E.
+# Si estado civil igual casado se paga por conyuge 200
+# F.
+# Por hijo se paga 1000 y por cada hijo a partir del 3ro
+# se paga por hijo un 40% mas del valor del hijo anterior.
+# Ejemplo: 1ro y 2do 1000 cada uno, el 3ro 1400
+# el 4to 1960, el 5to 2744, etc...
+# G.
 # Imprimir salario familiar.
 
-#nombre = ["Pablo","Ana","Jose","Maria","Tomas"]
-#sexo = ["M","F","M","F","M"]
-#horas = [170,200,160,320,250]
-#estcivil = ["","","","",""]
-#hijos = [0,0,0,0,0]
 
-empleado = []
-empleados = []
+MODO_PRUEBA = False
+
+if MODO_PRUEBA :
+	nombre = ["Pablo","Ana","Jose","Maria","Tomas"]
+	sexo = ["M","F","M","F","M"]
+	horas = [170,200,160,320,250]
+	estadoCivil = ["C","S","D","V","P"]
+	hijos = [6,0,2,3,1]
+	lista_empleados = []
+
+	for i in range(0,5) :
+		lista_datos = [nombre[i],sexo[i],horas[i],estadoCivil[i],hijos[i]]
+		lista_empleados.append(lista_datos)
+
+# Nombres de indices
 nombre = 0
 sexo = 1
 horas = 2
-estcivil = 3
+estadoCivil = 3
 hijos = 4
-valorhora = 800
-horarionormal = 160
-indicehoraextra = 1.5
 
-for i in range(0,5) :
-	empleado.append( input(f"Ingresar el nombre del empleado {i+1}\n") )
-	empleado.append( input(f"Ingresar el sexo del empleado {i+1}\n") )
-	empleado.append( int(input(f"Ingresar la cant. de horas trabajadas del empleado {i+1}\n")) )
-	empleado.append( input(f"Ingresar el estado civil del empleado {i+1}\n") )
-	empleado.append( int(input(f"Ingresar la cant. de hijos del empleado {i+1}\n")) )
-	empleados.append( empleado )
-	empleado = []
+# Parametros (valores estaticos)
+valorHora = 800
+horarioNormal = 160
+indice_HoraExtra = 1.5
+conyuge = 200
+asignacionPorHijo = 1000
+familiaNumerosa = 3
+indice_familiaNumerosa = 1.4
 
-for i in range(0,5) :
-	sueldobruto = 0
-	horasextras = 0
-	sueldoneto = 0
+# Mensajes de ingresos
+ingresarNombre = "Ingresar el nombre del empleado"
+ingresarSexo = "Ingresar el sexo del empleado"
+ingresarHoras = "Ingresar la cant. de horas trabajadas del empleado"
+ingresarEstadoCivil = "Ingresar el estado civil del empleado"
+ingresarHijos = "Ingresar la cant. de hijos del empleado"
 
-	print(f"Nombre: {empleados[i][nombre]}")
-	print(f"Genero: {empleados[i][sexo]}")
-	print(f"Horas trabajadas: {empleados[i][horas]}")	# A.
 
-	sueldobruto = empleados[i][horas] * valorhora
-	print( f"Sueldo bruto: {sueldobruto:,.2f}".replace(","," ") )	# B.
+# MODO_PRUEBA es para probar la logica sin las entradas por teclado
+if not MODO_PRUEBA :
 
-	horasextras = empleados[i][horas] - horarionormal
-	print(f"Horas extras: {horasextras}")	# C.
+	lista_empleados = []
+	for i in range(0,5) :
+		empleado = []
+		empleado.append( input(f"{ingresarNombre} {i+1}\n") )
+		empleado.append( input(f"{ingresarSexo} {i+1}\n") )
+		empleado.append( int(input(f"{ingresarHoras} {i+1}\n")) )
+		empleado.append( input(f"{ingresarEstadoCivil} {i+1}\n") )
+		empleado.append( int(input(f"{ingresarHijos} {i+1}\n")) )
+		lista_empleados.append( empleado )
+		print("-" * 50)
 
-	sueldoneto = sueldobruto + horasextras * indicehoraextra
-	print( f"Sueldo neto: {sueldoneto:,.2f}".replace(","," ") )		# D.
-	print("----------")
+
+print("\nLista de empleados de la empresa")
+print("=" * 50)
+
+# Como foreach en Java, seria "por cada empleado de la lista lista_empleados"
+for empleado in lista_empleados :
+	
+	sueldoBasico = 0
+	horasExtras = 0
+	sueldoFamiliar = 0
+
+	formato_sueldo = ''
+	totalHorasExtras = 0
+	asignacion = 0
+	cantidadHijos = 0
+	sueldoBruto = 0
+
+	# A.
+	print(f"Nombre: {empleado[nombre]}")
+	print(f"Genero: {empleado[sexo]}")
+	print(f"Horas trabajadas: {empleado[horas]}")
+
+	# B.
+	sueldoBasico = empleado[horas] * valorHora
+
+	formato_sueldo = f"$ {sueldoBasico:,.2f}".replace(","," ").replace (".00",".-")
+	print( f"Sueldo basico: {formato_sueldo}" )
+
+	# C.
+	horasExtras = empleado[horas] - horarioNormal
+
+	print(f"Horas extras: {horasExtras}")
+
+	# D.
+	totalHorasExtras = horasExtras * valorHora * indice_HoraExtra
+
+	if MODO_PRUEBA :
+		print(f"{'>' * 8} Total horas extras: $ {totalHorasExtras:,.2f}".replace(',',' ').replace('.00','.-'))
+	
+	formato_sueldo = f"$ {sueldoBasico + totalHorasExtras:,.2f}".replace(","," ").replace(".00",".-")
+	print( f"Sueldo basico mas horas extras: {formato_sueldo}" )
+
+	# E.
+	if empleado[estadoCivil] == "C" :
+		sueldoFamiliar += conyuge
+		if MODO_PRUEBA :
+			print(f"{'>' * 8} Asignacion conyuge: $ {conyuge:,.2f}".replace(',',' ').replace('.00','.-'))
+
+	# F.
+	for cantidadHijos in range(1,empleado[hijos]+1) :
+
+		if cantidadHijos >= familiaNumerosa :
+			asignacion = asignacion * indice_familiaNumerosa
+			if MODO_PRUEBA :
+				print(f"{'>'*8} Asignacion hijo {cantidadHijos}: $ {asignacion:,.2f}".replace(',',' ').replace('.00','.-'))
+		else :
+			asignacion = asignacionPorHijo
+			if MODO_PRUEBA :
+				print(f"{'>'*8} Asignacion hijo {cantidadHijos}: $ {asignacion:,.2f}".replace(',',' ').replace('.00','.-'))
+
+		sueldoFamiliar += asignacion
+
+	# G.
+	formato_sueldo = f"$ {sueldoFamiliar:,.2f}".replace(","," ").replace(".00",".-")
+	print( f"Sueldo familiar: {formato_sueldo}" )
+
+	if MODO_PRUEBA :
+		sueldoBruto = sueldoBasico + totalHorasExtras + sueldoFamiliar
+		formato_sueldo = f"$ {sueldoBruto:,.2f}".replace(","," ").replace(".00",".-")
+		print( f"{'>'*8} Sueldo bruto: {formato_sueldo}" )
+
+	print("-" * 50)
