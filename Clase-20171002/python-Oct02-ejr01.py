d6a2e4ae136cd77ce4e7daf49b4b937013cee362
CodeSeba <seba84x@gmail.com>
2017-10-02 14:35:19 -0300

    Arreglos varios

--- Clase-20171002/python-Oct02-ejr01.py
+++ Clase-20171002/python-Oct02-ejr01.py
@@ -1,18 +1,18 @@
 # Dado un curso de alumnos. Se deben
 # cargar las siguientes datos:
-# Nombre
-# Apellido
-# Nota 1
-# Nota 2
-# Nota 3
-# Genero
-# La carga finaliza con el nombre "pepe"
+# -Nombre
+# -Apellido
+# -Nota 1
+# -Nota 2
+# -Nota 3
+# -Genero
+# La carga finaliza con el nombre "pepe".
 #
-# Se debe calcular el promedio
-# si promedio < 4 entonces "Reprobado"
-# si promedio es >= 4 entonces "Aprobado"
+# Se debe calcular el promedio,
+# si el promedio < 4 entonces "Reprobado",
+# si el promedio es >= 4 entonces "Aprobado",
 # si aprobo los tres examenes con nota
-# superior a 7 entonces "Promocionado"
+# superior a 7 entonces "Promocionado".
 #
 # A. Indicar la cantidad de alumnos "Reprobados"
 # B. Nombre, Apellido, Promedio de los alumnos "Promocionados"
@@ -31,8 +31,8 @@
 # L. Nombre de los alumnos con la tercera nota mayor a 4 y menor a 7.
 
 lista_alumnos = []
-carga_alumno = []
-cont_alumnos = 0
+datos_alumno = []
+cant_alumnos = 0
 nombre = 0
 apellido = 1
 nota1 = 2
@@ -42,34 +42,44 @@ genero = 5
 promedio = 6
 estado = 7
 
-lista_alumnos = [["Jose","Saraza",2,2,2,"Masc"],["Ana","Salsa",9,9,8,"Feme"],["Pablo","Martillo",7,6,4,"Masc"],["Pepa","Pitt",2,3,3,"Feme"]]
-cont_alumnos = 4
+# Esto es para pruebas
+lista_alumnos.append(["Jose","Saraza",2,2,2,"Masc"])
+lista_alumnos.append(["Ana","Salsa",9,9,8,"Feme"])
+lista_alumnos.append(["Pablo","Martillo",7,6,4,"Masc"])
+lista_alumnos.append(["Pepa","Pitt",2,3,3,"Feme"])
+lista_alumnos.append(["Tomas","Tomate",2,2,2,"Masc"])
+lista_alumnos.append(["Ariadna","Menendez",10,10,10,"Feme"])
+cant_alumnos = 6
+
 '''
+print("Ingresar los datos del alumno",cant_alumnos+1)
 input_nombre = input("Ingresar Nombre\n")
 
 while input_nombre != "pepe" :
+
 	input_apellido = input("Ingresar Apellido\n")
 	input_nota1 = int( input("Ingresar Nota 1\n") )
 	input_nota2 = int( input("Ingresar Nota 2\n") )
 	input_nota3 = int( input("Ingresar Nota 3\n") )
 	input_genero = input("Ingresar Genero\n")
 
-	carga_alumno.append(input_nombre)
-	carga_alumno.append(input_apellido)
-	carga_alumno.append(input_nota1)
-	carga_alumno.append(input_nota2)
-	carga_alumno.append(input_nota3)
-	carga_alumno.append(input_genero)
+	datos_alumno.append(input_nombre)
+	datos_alumno.append(input_apellido)
+	datos_alumno.append(input_nota1)
+	datos_alumno.append(input_nota2)
+	datos_alumno.append(input_nota3)
+	datos_alumno.append(input_genero)
 
-	lista_alumnos.append(carga_alumno)
-	cont_alumnos += 1
-	carga_alumno = []
+	lista_alumnos.append(datos_alumno)
+	cant_alumnos += 1
+	datos_alumno = []
 
+	print("Ingresar los datos del alumno",cant_alumnos+1)
 	input_nombre = input("Ingresar Nombre\n")
-
-#print(lista_alumnos)
 '''
+
 for alumno in lista_alumnos :
+
 	calc_promedio = ( alumno[nota1] + alumno[nota2] + alumno[nota3] ) / 3
 	alumno.append(calc_promedio)
 
@@ -85,54 +95,55 @@ for alumno in lista_alumnos :
 
 	alumno.append(alumno_estado)
 
-cont_reprobados = 0
-cont_mujeresAprobadas = 0
 
-print("Lista de Alumnos Promocionados:\n")
+cant_reprobados = 0
+cant_mujeresAprobadas = 0
+
+print("Lista de Alumnos Promocionados:")
 
 for alumno in lista_alumnos :
 	
 	if alumno[estado] == "Reprobado" :
-		cont_reprobados += 1
+		cant_reprobados += 1
 	
 	if alumno[estado] == "Promocionado" :
 		print("Nombre:",alumno[nombre])
 		print("Apellido:",alumno[apellido])
 		print("Promedio:",alumno[promedio])
-		print("\n")
+		print("-" * 50)
 
 	if alumno[genero] == "Feme" :
 		if alumno[estado] == "Aprobado" or alumno[estado] == "Promocionado" :
-			cont_mujeresAprobadas += 1
+			cant_mujeresAprobadas += 1
 
 
-print("Cantidad de Reprebados:",cont_reprobados)
-print("Cantidad de Mujeres Aprobados:",cont_mujeresAprobadas)
+print("Cantidad de Reprebados:",cant_reprobados)
+print("Cantidad de Mujeres Aprobados:",cant_mujeresAprobadas)
 
-cont_feme = 0
-cont_masc = 0
-cont_promo = 0
+cant_feme = 0
+cant_masc = 0
+cant_promo = 0
 acum_promedio = 0
 
 for alumno in lista_alumnos :
 	
 	if alumno[genero] == "Feme" :
-		cont_feme += 1
+		cant_feme += 1
 
 	if alumno[genero] == "Masc" :
-		cont_masc += 1
+		cant_masc += 1
 
 	if alumno[estado] == "Promocionado":
-		cont_promo += 1
+		cant_promo += 1
 
 	acum_promedio += alumno[promedio]
 
-promedioGeneral = acum_promedio / cont_alumnos
+promedioGeneral = acum_promedio / cant_alumnos
 
-porcetajeReprobados = cont_reprobados * 100 / cont_alumnos
+porcetajeReprobados = cant_reprobados * 100 / cant_alumnos
 
-print("Cantidad de Alumnos Mujeres:",cont_feme)
-print("Cantidad de Alumnos Hombres:",cont_masc)
-print("Cantidad de Alumnos promocionados:",cont_promo)
+print("Cantidad de Alumnos Mujeres:",cant_feme)
+print("Cantidad de Alumnos Hombres:",cant_masc)
+print("Cantidad de Alumnos promocionados:",cant_promo)
 print("Promedio General:",promedioGeneral)
-print("Porcentaje Reprobados:",porcetajeReprobados)
+print("Porcentaje Reprobados:",porcetajeReprobados,"%")
