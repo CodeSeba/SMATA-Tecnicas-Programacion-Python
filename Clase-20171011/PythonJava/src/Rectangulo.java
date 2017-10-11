2f8df85abb52ba7d2b2e26ece3ea5a44bb3dac6b
CodeSeba <seba84x@gmail.com>
2017-10-11 13:26:36 -0300

    Basico de Java

---
+++ Clase-20171011/PythonJava/src/Rectangulo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Rectangulo {
+	public static void main (String[] args) {
+		double l1 = 0;
+		double l2 = 0;
+		double perimetro = 0;
+		double area = 0;
+		Scanner teclado = new Scanner( System.in );
+
+		System.out.println("Ingresar lado 1");
+		l1 = teclado.nextDouble();
+		System.out.println("Ingresar lado 2");
+		l2 = teclado.nextDouble();
+		
+		perimetro = 2 * ( l1 + l2 );
+		area = l1 * l2;
+
+		System.out.println("El perimetro del Rectangulo es: " + perimetro);
+		System.out.println("El area del Rectangulo: " + area);
+	}
+}
