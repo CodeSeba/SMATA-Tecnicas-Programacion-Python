320c13a362d873f46138ec08921b26da9c54a0eb
CodeSeba <seba84x@gmail.com>
2017-10-25 23:57:19 -0300

    Agregado python usando clase y metodos

---
+++ Clase-20171025/python-Oct25-ejr01.py
@@ -0,0 +1,53 @@
+# Ingresar el nombre de un alumno y sus tres notas por teclado.
+# Generar los metodos que:
+# -Carguen los datos.
+# -Imprimir los datos.
+# -Calcular el promedio.
+# -Imprimir nombre, promedio y si aprobo o no.
+
+
+# Inicio de clase Alumno
+class Alumno:
+	nombre = ""
+	notas = []
+	promedio = 0
+
+	def cargarDatos(self):
+		self.nombre = input("Ingresar Nombre:\n")
+		
+		for i in range(3):
+			self.notas.append( int(input("Ingresar Nota "+str(i+1)+":\n")) )
+
+		print("")
+
+	def imprimirDatos(self):
+		print("Nombre:", self.nombre)
+		print("Notas: ",end="")
+		for unaNota in self.notas: print(unaNota,end=", ")
+		print("\n")
+
+	def calcularPromedio(self):
+		cant_notas = len(self.notas)
+
+		for unaNota in self.notas:
+			self.promedio += unaNota
+
+		self.promedio /= cant_notas
+
+	def imprimirInforme(self):
+		print("Alumno:", self.nombre)
+		print("Promedio:", round(self.promedio,2))
+
+		estado = "Aprobado" if self.promedio > 4 else "A Marzo"
+
+		print("Estado:",estado)
+
+# Fin de clase Alumno
+
+# Main
+unAlumno = Alumno()
+
+unAlumno.cargarDatos()
+unAlumno.imprimirDatos()
+unAlumno.calcularPromedio()
+unAlumno.imprimirInforme()
